dc371891ee0d75fb9035f31e1c2bfa181544bc32
dhubleur <dhubleur@student.42.fr>
2023-11-01 13:47:37 +0100

    colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-// To use instead of printf because to use printf, we need to use some double quotes
-// To use instead of puts because puts adds a newline at the end
+/*
+	To use instead of printf because to use printf, we need to use some double quotes
+	To use instead of puts because puts adds a newline at the end
+*/
 void print_string(char *str) {
 	for (int i = 0; str[i]; i++) {
 		putchar(str[i]);
@@ -9,17 +11,20 @@ void print_string(char *str) {
 }
 
 int main() {
-	// Define some characters
+	/*
+		Define some characters
+	*/
 	char quote = 34;
 	char newline = 10;
 	char comma = 44;
 
-	// Define the code
-	char code[42][100] = {
+	char code[44][100] = {
 		"#include <stdio.h>",
 		"",
-		"// To use instead of printf because to use printf, we need to use some double quotes",
-		"// To use instead of puts because puts adds a newline at the end",
+		"/*",
+		"	To use instead of printf because to use printf, we need to use some double quotes",
+		"	To use instead of puts because puts adds a newline at the end",
+		"*/",
 		"void print_string(char *str) {",
 		"	for (int i = 0; str[i]; i++) {",
 		"		putchar(str[i]);",
@@ -27,32 +32,32 @@ int main() {
 		"}",
 		"",
 		"int main() {",
-		"	// Define some characters",
+		"	/*",
+		"		Define some characters",
+		"	*/",
 		"	char quote = 34;",
 		"	char newline = 10;",
 		"	char comma = 44;",
 		"",
-		"	// Define the code",
-		"	char code[42][100] = {",
+		"	char code[44][100] = {",
 		"		",
 		"	};",
 		"",
-		"	// Print the code",
-		"	for (int i = 0; i < 18; i++) {",
+		"	for (int i = 0; i < 21; i++) {",
 		"		print_string(code[i]);",
 		"		putchar(newline);",
 		"	}",
-		"	for (int i = 0; i < 42; i++) {",
-		"		print_string(code[18]);",
+		"	for (int i = 0; i < 44; i++) {",
+		"		print_string(code[21]);",
 		"		putchar(quote);",
 		"		print_string(code[i]);",
 		"		putchar(quote);",
-		"		if (i < 41) {",
+		"		if (i < 43) {",
 		"			putchar(comma);",
 		"		}",
 		"		putchar(newline);",
 		"	}",
-		"	for (int i = 19; i < 42; i++) {",
+		"	for (int i = 22; i < 44; i++) {",
 		"		print_string(code[i]);",
 		"		putchar(newline);",
 		"	}",
@@ -60,22 +65,21 @@ int main() {
 		"}"
 	};
 
-	// Print the code
-	for (int i = 0; i < 18; i++) {
+	for (int i = 0; i < 21; i++) {
 		print_string(code[i]);
 		putchar(newline);
 	}
-	for (int i = 0; i < 42; i++) {
-		print_string(code[18]);
+	for (int i = 0; i < 44; i++) {
+		print_string(code[21]);
 		putchar(quote);
 		print_string(code[i]);
 		putchar(quote);
-		if (i < 41) {
+		if (i < 43) {
 			putchar(comma);
 		}
 		putchar(newline);
 	}
-	for (int i = 19; i < 42; i++) {
+	for (int i = 22; i < 44; i++) {
 		print_string(code[i]);
 		putchar(newline);
 	}
